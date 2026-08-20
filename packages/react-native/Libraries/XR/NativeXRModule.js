46f07d0d0daedd9950989852e901cf3847a7375e
Eduardo Dorantes <eduardo.dorantes.07@gmail.com>
2026-08-20 14:26:24 -0600

    feat: add XR module for immersive spaces

---
+++ packages/react-native/Libraries/XR/NativeXRModule.js
@@ -0,0 +1,9 @@
+/**
+ * @flow strict
+ * @format
+ */
+
+import NativeXRModule from '../../visionos_modules/NativeXRModule';
+
+export * from '../../visionos_modules/NativeXRModule';
+export default NativeXRModule;
